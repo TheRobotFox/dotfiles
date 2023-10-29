a59d023d0d24b45ffa871cceffabda3ed68143fa
Tomke Pfoch <pfoch.tomke@gmail.com>
2023-10-29 17:22:59 +0100

    init

---
+++ i3/brightness_up.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+step=20
+divider=1.5
+file=~/.config/i3/brightness
+nightmode=$(( $(cat file) +1))
+nightmode=$(echo "($nightmode>$step)*$step+($nightmode<=$step)*$nightmode" | bc)
+
+red=$(echo "1+($nightmode)/$step/$divider" | bc -l)
+brightness=$(echo "1-($nightmode)/$step" | bc -l)
+
+echo $nightmode $red $brightness
+xrandr --output DP-0 --brightness $brightness  --gamma $red:1:1 --output DP-1-1 --brightness $brightness --gamma $red:1:1
+echo $nightmode > file
