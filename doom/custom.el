7b4437fead2e0830099703b2b5dc21c43a4a968b
Tomke Pfoch <pfoch.tomke@gmail.com>
2023-11-07 20:13:21 +0100

    Markdown export, Light

--- doom/custom.el
+++ doom/custom.el
@@ -4,7 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+<<<<<<< HEAD
  '(org-agenda-files '("~/org/tu/mfmt.org"))
+=======
+ '(org-agenda-files
+   '("~/org/notes/Setup.org" "/home/new/projects/MLCC/MLCC.org"))
+ '(org-export-backends '(ascii html icalendar latex man md odt))
+>>>>>>> abc3255 (Markdown export, Light)
  '(warning-suppress-types '((initialization) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -13,3 +19,4 @@
  ;; If there is more than one, they won't work right.
  '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
 (put 'narrow-to-region 'disabled nil)
+(put 'customize-option 'disabled nil)
