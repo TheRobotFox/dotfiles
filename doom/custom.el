3802fe8b49bb7ce253b5516aec43b5b42c40abbc
Tomke <pfoch.tomke@gmail.com>
2023-11-16 17:47:58 +0100

    Agenda

--- doom/custom.el
+++ doom/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(org-agenda-files '("~/org/tu/mfmt.org"))
+ '(org-agenda-files
+   '("~/org/tu/EI/Tut4.org" "/home/new/org/tu/RO/Tut3.org" "/home/new/org/tu/mfmt.org"))
  '(warning-suppress-types '((initialization) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
