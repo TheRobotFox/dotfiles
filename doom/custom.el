9237f63ba9d5a43cb86454e8f860cd69e15ebada
Tomke <pfoch.tomke@gmail.com>
2024-01-22 15:46:14 +0100

    Clangd

--- doom/custom.el
+++ doom/custom.el
@@ -3,12 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(lsp-inlay-hint-enable t)
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
    '("~/org/todo.org" "/home/new/org/roam/20231130143243-trie_baum.org" "/home/new/org/roam/20231114081552-digital_entwurf.org" "/home/new/org/tu/RO/VL4.org" "/home/new/org/tu/EI/Tut4.org" "/home/new/org/tu/RO/Tut3.org" "/home/new/org/tu/mfmt.org"))
  '(org-export-backends '(ascii html icalendar latex md odt reveal))
  '(org-latex-packages-alist '(("inkscapelatex=false" "svg" nil nil)))
- '(warning-suppress-types '((initialization) (defvaralias))))
+ '(warning-suppress-types '((initialization) (defvaralias)) t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
