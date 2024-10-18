8655989aa8e37907be619ed09765f00e4dd871b4
Tomke Pfoch <pfoch.tomke@gmail.com>
2024-10-18 12:36:16 +0200

    src inline

--- doom/config.el
+++ doom/config.el
@@ -126,6 +126,11 @@
  'org-babel-load-languages
  '((dot . t)))
 
+  ;; Latex TOC pagebreak
+  (setq org-latex-toc-command "\\tableofcontents \\clearpage")
+(add-to-list 'org-latex-packages-alist
+             '("AUTO" "babel" t ("pdflatex" "xelatex" "lualatex")))
+
   (setq org-babel-C-compiler "gcc -std=c2x -lm -I .")
   (setq org-babel-C++-compiler "g++ -std=c++23 -I .")
 
@@ -427,4 +432,3 @@
         :sqrt "sqrt"
         :infinity "INFINITY"
         :uint "unsigned int")
-()
