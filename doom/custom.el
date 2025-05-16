609adc1f5b8420fe42fba66adb1ae073b06ff0dd
Tomke <pfoch.tomke@gmail.com>
2025-05-16 14:53:34 +0200

    doom kill

--- doom/custom.el
+++ doom/custom.el
@@ -3,10 +3,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default))
  '(lsp-clangd-download-url
    "https://github.com/clangd/clangd/releases/download/19.1.2/clangd-linux-19.1.2.zip")
  '(lsp-clangd-version "19.1.2")
- '(org-latex-packages-alist '(("" "minted" nil nil)))
+ '(org-agenda-files
+   '("~/org/roam/20250424095209-datenbank.org"
+     "/home/new/org/roam/20250415124329-stochastik.org"
+     "/home/new/org/journal.org" "/home/new/org/notes.org"
+     "/home/new/org/projects.org" "/home/new/org/todo.org"))
+ '(org-latex-default-packages-alist
+   '(("" "unicode-math" nil nil) ("" "amsmath" t ("lualatex" "xetex"))
+     ("" "fontspec" t ("lualatex" "xetex")) ("AUTO" "inputenc" t ("pdflatex"))
+     ("T1" "fontenc" t ("pdflatex")) ("" "graphicx" t nil)
+     ("" "longtable" nil nil) ("" "wrapfig" nil nil) ("" "rotating" nil nil)
+     ("normalem" "ulem" t nil) ("" "amsmath" t ("pdflatex"))
+     ("" "amssymb" t ("pdflatex")) ("" "capt-of" nil nil)
+     ("" "hyperref" nil nil)))
+ '(org-latex-packages-alist '(("" "minted" nil nil) ("" "tikz" nil nil)))
  '(org-latex-pdf-process
    '("latexmk -f -pdf -%latex -interaction=nonstopmode -output-directory=%o %f -shell-escape"))
  '(org-latex-to-mathml-convert-command "latexmlmath %i --presentationmathml=%o")
--- doom/packages.el
+++ doom/packages.el
@@ -57,3 +57,28 @@
 (package! ox-gfm :recipe (:host github :repo "larstvei/ox-gfm"))
 (package! ox-reveal :recipe (:host github :repo "yjwen/org-reveal"))
 (package! org-present :recipe (:host github :repo "rlister/org-present"))
+(package! org :recipe
+  (:host nil :repo "https://git.tecosaur.net/mirrors/org-mode.git" :remote "mirror" :fork
+         (:host nil :repo "https://git.tecosaur.net/tec/org-mode.git" :branch "dev" :remote "tecosaur")
+         :files
+         (:defaults "etc")
+         :build t :pre-build
+         (with-temp-file "org-version.el"
+           (require 'lisp-mnt)
+           (let
+               ((version
+                 (with-temp-buffer
+                   (insert-file-contents "lisp/org.el")
+                   (lm-header "version")))
+                (git-version
+                 (string-trim
+                  (with-temp-buffer
+                    (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+                    (buffer-string)))))
+             (insert
+              (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+              (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+              "(provide 'org-version)\n"))))
+  :pin nil)
+
+(unpin! org)
